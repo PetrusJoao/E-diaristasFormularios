1332d305dad43d5e82f00981f116101318d1cc52
João Pedro <joaopetrussousa@gmail.com>
2021-09-08 17:34:09 -0300

    Creating API

--- app/Http/Controllers/BuscaDiaristaCep.php
+++ app/Http/Controllers/BuscaDiaristaCep.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Diarista;
 use App\Services\ViaCep;
 use Illuminate\Http\Request;
 
@@ -17,6 +18,14 @@ class BuscaDiaristaCep extends Controller
     {
         $endereco = $viaCep->buscar($request->cep);
 
-        dd($endereco['ibge']);
+        if($endereco === false){
+            return response()->json(['erro'=>'CEP inválido'], 400);
+        }
+
+        return [
+            'diaristas' => Diarista::buscaPorCodigoIbge($endereco['ibge']),
+            'quantidade_diaristas' => Diarista::quantidadePorCodigoIbge($endereco['ibge'])
+        ];
+        
     }
 }
--- app/Models/Diarista.php
+++ app/Models/Diarista.php
@@ -9,6 +9,47 @@ class Diarista extends Model
 {
     use HasFactory;
 
+    /**
+     * Definem os campos que podem ser gravados
+     *
+     * @var array
+     */
     protected $fillable = ['nome_completo', 'cpf', 'email', 'telefone', 'logradouro', 'numero', 
     'complemento', 'bairro', 'cidade', 'estado', 'cep', 'codigo_ibge', 'foto_usuario'];
+
+    /**
+     * Define os campos usados na serialização
+     *
+     * @var array
+     */
+    protected $visible = ['nome_completo', 'cidade', 'foto_usuario'];
+
+    public function getFotoUsuarioAttribute(string $valor)
+    {
+        return config('app.url') . '/' . $valor;
+    }
+
+    /**
+     * Busca diaristas por código ibge
+     *
+     * @param integer $codigoIbge
+     * @return void
+     */
+    static public function buscaPorCodigoIbge(int $codigoIbge)
+    {
+        return self::where('codigo_ibge', $codigoIbge)->limit(6)->get();
+    }
+
+    /**
+     * Retorna a quantidade de diaristas
+     *
+     * @param integer $codigoIbge
+     * @return void
+     */
+    static public function quantidadePorCodigoIbge(int $codigoIbge)
+    {
+        $quantidade = self::where('codigo_ibge', $codigoIbge)->count();
+
+        return $quantidade > 6 ? $quantidade - 6 : 0;
+    }
 }
